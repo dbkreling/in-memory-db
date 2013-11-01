8d54c465fa5fa9fd00273bafa492d7f7a8fd58c0
Daniel Kreling <dbkreling@gmail.com>
2013-11-01 17:14:11 -0200

    Including get_member_number function
    Get_member_number function returns the value of the name the user
    is searching.

--- database.c
+++ database.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 
 typedef struct member{
     char name[32];
@@ -7,12 +8,27 @@ typedef struct member{
     struct member * next;
 } t_member;
 
+// declare function get that returns the member value
+int get_member_number(char * name, t_member * list){
+	t_member * current;
+	current = list;
+	while (current != NULL){
+		if (!strcmp(name,current->name)){
+			printf ("The value is %d\n", current->value);
+			return 0;
+		}
+		current = current->next;
+	}
+	printf("There is no %s in the list\n", name);
+	return 0;
+}
 
 int main(int argc, char *argv[]){
-	t_member *var = NULL;
-	var = (t_member *) malloc(sizeof(t_member));
-	var->value = 10;
-	printf ("%d\n", var->value);
+	t_member * list = NULL;
+	list = (t_member *) malloc(sizeof(t_member));
+	list->value = 1;
+	strcpy(list->name,"ana");
+	get_member_number("ana",list);
 	return 0;
 } 
 
